f3406d8dada3b9693f2c17efa604664b2a98d5cf
Alfaizer <alfaizerc@gmail.com>
2025-02-20 20:31:45 +0800

    Added AboutMe.jsx and AboutMe.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Hero from "./components/Hero";
+import AboutMe from "./pages/AboutMe";
+import Resume from "./pages/Resume";
+import Links from "./pages/Links";
 import './App.css';
-import Hero from './components/Hero';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      {}
-      <Hero />
-
-      <div>
-        
-      </div>
-     
-      
-      
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/about-me" element={<AboutMe />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/links" element={<Links />} />
+      </Routes>
+    </Router>
   );
 }
 
